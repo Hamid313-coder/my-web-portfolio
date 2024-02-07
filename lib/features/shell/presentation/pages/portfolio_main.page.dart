ee6aa0e7d850d925b4cf588dab57027b9359af0e
hamid313-coder <hamid.hussainy0@gmail.com>
2024-02-07 12:28:13 +0430

    intergrate shell navigation done

--- lib/features/shell/presentation/pages/portfolio_main.page.dart
+++ lib/features/shell/presentation/pages/portfolio_main.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/features/navigation/presentation/widgets/left_navigation.dart';
 import 'package:my_portfolio/helpers/utils.dart';
+import 'package:my_portfolio/styles/colors.dart';
 
 class PortfolioMainPage extends StatelessWidget {
   const PortfolioMainPage({required this.child, super.key});
@@ -8,6 +10,15 @@ class PortfolioMainPage extends StatelessWidget {
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(key: Utils.mainScaffold, body: child);
+    return Scaffold(
+        key: Utils.mainScaffold,
+        backgroundColor: PersonalPortfolioColors.mainBlue,
+        body: Stack(children: [
+          Center(child: child),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: LeftNavigation(),
+          ),
+        ]));
   }
 }

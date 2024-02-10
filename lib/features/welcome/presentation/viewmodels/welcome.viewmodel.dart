426f0010f44c6a05d4f3ec8b6f162b826860bfe8
hamid313-coder <hamid.hussainy0@gmail.com>
2024-02-10 17:21:02 +0430

    added greeting lable

--- lib/features/welcome/presentation/viewmodels/welcome.viewmodel.dart
+++ lib/features/welcome/presentation/viewmodels/welcome.viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_portfolio/features/welcome/data/models/welcome_page.model.dart';
 import 'package:my_portfolio/features/welcome/data/repositories/iwelcome.repository.dart';
+import 'package:my_portfolio/features/welcome/presentation/providers/welcom_page.providers.dart';
 
 class WelcomePageVieModel {
   final Ref ref;
@@ -10,6 +11,8 @@ class WelcomePageVieModel {
 
   Future<WelcomePageModel> getWelcomePageData() async {
     final welcomePageData = await welcomeRepository.getWelcomePageData();
+    ref.read(greetingsRawListProvider.notifier).state =
+        welcomePageData.greetings;
     return welcomePageData;
   }
 }

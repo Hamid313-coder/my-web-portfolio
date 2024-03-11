8e835268a2ea1ab5495c1e6639f95b5386357c87
hamid313-coder <hamid.hussainy0@gmail.com>
2024-03-11 09:44:41 +0430

    added saving last visited page in localstorage

--- lib/features/navigation/presentation/viewmodels/left_navigation.viewmodel.dart
+++ lib/features/navigation/presentation/viewmodels/left_navigation.viewmodel.dart
@@ -10,8 +10,6 @@ class LeftNavigationViewModel extends StateNotifier<List<LeftNavigationItem>> {
   LeftNavigationViewModel(List<LeftNavigationItem> items, this.ref)
       : super([]) {
     state = items;
-    final item = items.first;
-    selectNavItem(item);
   }
 
   void selectNavItem(LeftNavigationItem item) {
@@ -25,5 +23,18 @@ class LeftNavigationViewModel extends StateNotifier<List<LeftNavigationItem>> {
       for (final element in state)
         element.copyWith(isSelected: element == item),
     ];
+
+    ref.read(webLocalStorageProvider).storeSelectedNav(item.route);
+  }
+
+  void init() {
+    var selectedNavRoute = ref.read(webLocalStorageProvider).getSelectedNav();
+
+    if (selectedNavRoute.isNotEmpty) {
+      var item = state.where((i) => i.route == selectedNavRoute).first;
+      selectNavItem(item);
+    } else {
+      selectNavItem(state.first);
+    }
   }
 }
--- lib/shared/providers/share_providers.dart
+++ lib/shared/providers/share_providers.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_portfolio/features/welcome/presentation/pages/welcom.page.dart';
+import 'package:my_portfolio/services/local_storage.service.dart';
 import 'package:my_portfolio/shared/repositories/database_provider.dart';
 import 'package:my_portfolio/styles/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final pageColorProvider = StateProvider<LinearGradient>((ref) {
   return PersonalPortfolioColors.pageColor[WelcomePage.route]!;
@@ -19,4 +21,12 @@ final firebaseDbInstanceProvider = Provider<FirebaseFirestore>((ref) {
 
 final dbProvider = Provider<DatabaseProvider>((ref) {
   return DatabaseProvider(ref);
+});
+
+final sharedPrefsInstanceProvider = Provider((ref) {
+  return SharedPreferences.getInstance();
+});
+
+final webLocalStorageProvider = Provider((ref) {
+  return WebLocalStorage(ref);
 });

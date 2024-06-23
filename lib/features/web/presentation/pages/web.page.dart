a3e57d57ce4c06a57eb67dd74d8219e8e4abf7f6
hamid313-coder <hamid.hussainy0@gmail.com>
2024-06-23 10:22:17 +0430

    web page responsive done

--- lib/features/web/presentation/pages/web.page.dart
+++ lib/features/web/presentation/pages/web.page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_portfolio/features/web/presentation/providers/web.providers.dart';
+import 'package:my_portfolio/features/web/presentation/responsiveness/web_page_responsive.config.dart';
+import 'package:my_portfolio/helpers/responsive_ui_helper.dart';
 import 'package:my_portfolio/shared/widgets/error_notification.dart';
 import 'package:my_portfolio/styles/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -13,6 +15,8 @@ class WebPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final uiConfig = WebPageResponsiveConfig
+        .responsiveUI[ResponsiveUIHelper.getDeviceType(context)]!;
     final webAsyncData = ref.watch(webProvider);
     return webAsyncData.when(
         loading: () => const Center(
@@ -26,7 +30,8 @@ class WebPage extends ConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Icon(webData.icon,
-                        size: 80, color: PersonalPortfolioColors.webIcon)
+                        size: uiConfig.iconSize,
+                        color: PersonalPortfolioColors.webIcon)
                     .animate(onPlay: (controller) {
                   controller.repeat(reverse: true);
                 }).scaleXY(
@@ -36,23 +41,27 @@ class WebPage extends ConsumerWidget {
                         curve: Curves.easeInOut),
                 Text(webData.title,
                     textAlign: TextAlign.center,
-                    style: const TextStyle(
+                    style: TextStyle(
                         color: Colors.white,
-                        fontSize: 100,
+                        fontSize: uiConfig.titleSize,
                         fontWeight: FontWeight.bold)),
                 const SizedBox(height: 10),
-                GestureDetector(
-                  onTap: () => launchUrl(Uri.parse(webData.url)),
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35),
-                        color: Colors.white.withOpacity(0.1)),
-                    child: Text(
-                      webData.subtitle,
-                      style: const TextStyle(
-                          fontSize: 20, color: PersonalPortfolioColors.webIcon),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () => launchUrl(Uri.parse(webData.url)),
+                    child: Container(
+                      margin: uiConfig.buttonMargin,
+                      padding: uiConfig.buttonPadding,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(35),
+                          color: Colors.white.withOpacity(0.1)),
+                      child: Text(
+                        webData.subtitle,
+                        style: TextStyle(
+                            fontSize: uiConfig.buttonLabelSize,
+                            color: PersonalPortfolioColors.webIcon),
+                      ),
                     ),
                   ),
                 )
--- lib/features/web/presentation/responsiveness/web_page_responsive.config.dart
+++ lib/features/web/presentation/responsiveness/web_page_responsive.config.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/helpers/enums.dart';
 
-class LinkedInPageResponsiveConfig {
+class WebPageResponsiveConfig {
   final double iconSize;
   final double titleSize;
   final double buttonLabelSize;
   final EdgeInsets buttonPadding;
   final EdgeInsets buttonMargin;
 
-  LinkedInPageResponsiveConfig(
+  WebPageResponsiveConfig(
       {required this.iconSize,
       required this.titleSize,
       required this.buttonLabelSize,
       required this.buttonMargin,
       required this.buttonPadding});
 
-  static Map<DeviceType, LinkedInPageResponsiveConfig> responsiveUI = {
-    DeviceType.mobile: LinkedInPageResponsiveConfig(
+  static Map<DeviceType, WebPageResponsiveConfig> responsiveUI = {
+    DeviceType.mobile: WebPageResponsiveConfig(
       iconSize: 40,
       titleSize: 60,
       buttonLabelSize: 20,
       buttonMargin: const EdgeInsets.only(top: 20),
       buttonPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
     ),
-    DeviceType.tablet: LinkedInPageResponsiveConfig(
+    DeviceType.tablet: WebPageResponsiveConfig(
       iconSize: 60,
       titleSize: 80,
       buttonLabelSize: 20,
       buttonMargin: const EdgeInsets.only(top: 20),
       buttonPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
     ),
-    DeviceType.desktop: LinkedInPageResponsiveConfig(
+    DeviceType.desktop: WebPageResponsiveConfig(
       iconSize: 80,
       titleSize: 100,
       buttonLabelSize: 30,
